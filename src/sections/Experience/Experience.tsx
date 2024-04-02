3036665a23bb6663305b08c8075dc95911f85471
Eddi3MS <edsonmarc2014@gmail.com>
2024-04-02 19:22:04 -0300

    update experience

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -16,23 +16,6 @@ const Experience = () => {
         time="03/22 à 07/23"
         title="Desenvolvedor Frontend - Teamsoft"
       />
-
-      <ExperienceCard
-        badges={[
-          'comunicação',
-          'tenacidade',
-          'resolução de problemas',
-          'criatividade',
-        ]}
-        desc="Trabalhei com vendas desde os meus 18 anos, onde pude desenvolver
-        diversas soft skills que agregam no desenvolvimento de software, como
-        comunicação, resolução de problemas, tenacidade e criatividade. Os
-        últimos dois anos desse período, passei estudando desenvolvimento de
-        software em paralelo ao trabalho."
-        listTitle="Soft Skills:"
-        time="01/08 à 12/21"
-        title="Vendedor"
-      />
     </Section>
   )
 }
